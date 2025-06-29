e5f616af66c77f043e2da9c52cab5235b03eca87
Hasindu Udara <hasindutechschool@gmail.com>
2025-06-29 13:57:39 +0530

    Create MainDashboard.fxml and MainDashboardController

---
+++ src/main/java/lk/hasindu/spiceshop/controller/MainDashboardController.java
@@ -0,0 +1,40 @@
+package lk.hasindu.spiceshop.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class MainDashboardController {
+
+    @FXML
+    private AnchorPane AnchorPaneLoadDashboard;
+
+    @FXML
+    private Button btnReportsDashboard;
+
+    @FXML
+    private Button btnSalesDashboard;
+
+    @FXML
+    private Button btnSatockDashBoard;
+
+    @FXML
+    private AnchorPane mainDashboard;
+
+    @FXML
+    void btnReportsDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSalesDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSatockDashBoardOnAction(ActionEvent event) {
+
+    }
+
+}
