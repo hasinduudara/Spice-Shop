3795d8469e0c452c1435ea949b06d5a3b1c8e16c
Hasindu Udara <hasindutechschool@gmail.com>
2025-06-29 12:54:49 +0530

    Update Welcome page UI and controller

--- src/main/java/lk/hasindu/spiceshop/controller/WelcomeController.java
+++ src/main/java/lk/hasindu/spiceshop/controller/WelcomeController.java
@@ -1,11 +1,21 @@
 package lk.hasindu.spiceshop.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 
 public class WelcomeController {
 
     @FXML
+    private Button btnGetStart;
+
+    @FXML
     private AnchorPane welcomePage;
+
+    @FXML
+    void btnGetStartOnAction(ActionEvent event) {
+
+    }
 
 }
